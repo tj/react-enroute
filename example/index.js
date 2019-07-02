7cf7ea123b637b3208effef3d6593f5772a840ed
farwayer <farwayer@gmail.com>
2019-07-02 15:42:54 +0300

    up example

--- example/index.js
+++ example/index.js
@@ -1,74 +1,51 @@
-
-import React, { Component } from 'react'
+import React, {createContext, useState, useEffect, useCallback, useContext} from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route } from '../index'
+import {Router, Route} from '../index'
 
 // note this is just an example, this package does not provide
 // a Link equivalent found in react-router, nor does it provide
 // bindings for tools like Redux. You'll need to wire these up
 // as desired.
-function Link({ to, children }) {
-  function click(e) {
+
+const navigateContext = createContext()
+
+function Link({to, children}) {
+  const navigate = useContext(navigateContext)
+
+  const click = useCallback(e => {
     e.preventDefault()
     navigate(to)
-  }
+  }, [])
 
   return <a href={to} onClick={click}>
     {children}
   </a>
 }
 
-class Index extends Component {
-  render() {
-    const { children } = this.props
-    return <div>
-      <h1>Pet List</h1>
-      <p>At least it is not a to-do list. Check out <Link to="/users">users</Link> or <Link to="/pets">pets</Link>.</p>
-      {children}
-    </div>
-  }
+function Index({children}) {
+  return <div>
+    <h1>Pet List</h1>
+    <p>At least it is not a to-do list. Check out <Link
+      to="/users">users</Link> or <Link to="/pets">pets</Link>.</p>
+    {children}
+  </div>
 }
 
-// function Index({ children }) {
-//   return <div>
-//     <h1>Pet List</h1>
-//     <p>At least it is not a to-do list. Check out <Link to="/users">users</Link> or <Link to="/pets">pets</Link>.</p>
-//     {children}
-//   </div>
-// }
-
-class Users extends Component {
-  render() {
-    const { users, children } = this.props
-    return <div>
-      <h2>Users</h2>
-      <ul>
-        {users.map(user => {
-          return <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
-          </li>
-        })}
-      </ul>
-      {children}
-    </div>
-  }
+function Users({users, children}) {
+  return <div>
+    <h2>Users</h2>
+    <ul>
+      {users.map(user => {
+        return <li key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      })}
+    </ul>
+    {children}
+  </div>
 }
 
-// function Users({ users, children }) {
-//   return <div>
-//     <h2>Users</h2>
-//     <ul>
-//       {users.map(user => {
-//         return <li key={user.id}>
-//           <Link to={`/users/${user.id}`}>{user.name}</Link>
-//         </li>
-//       })}
-//     </ul>
-//     {children}
-//   </div>
-// }
-
-function User({ user, pets }) {
+function User({user, pets}) {
   return <div>
     <p>{user.name} has {pets.length} pets:</p>
     <ul>
@@ -82,15 +59,15 @@ function User({ user, pets }) {
 }
 
 User = (fn => {
-  return ({ users, pets, params: { id } }) => {
+  return ({users, pets, params: {id}}) => {
     return fn({
-      user: users.filter(u => u.id == id)[0],
-      pets: pets.filter(p => p.user_id == id)
+      user: users.filter(u => u.id === id)[0],
+      pets: pets.filter(p => p.user_id === id),
     })
   }
 })(User)
 
-function Pets({ pets, children }) {
+function Pets({pets, children}) {
   return <div>
     <h2>Pets</h2>
     <ul>
@@ -104,15 +81,16 @@ function Pets({ pets, children }) {
   </div>
 }
 
-function Pet({ user, pet }) {
-  return <p>{pet.name} is a {pet.species} and is owned by <Link to={`/users/${user.id}`}>{user.name}</Link>.</p>
+function Pet({user, pet}) {
+  return <p>{pet.name} is a {pet.species} and is owned by <Link
+    to={`/users/${user.id}`}>{user.name}</Link>.</p>
 }
 
 Pet = (fn => {
-  return ({ users, pets, params: { id } }) => {
-    const pet = pets.filter(p => p.id == id)[0]
-    const user = users.filter(u => u.id == pet.user_id)[0]
-    return fn({ user, pet })
+  return ({users, pets, params: {id}}) => {
+    const pet = pets.filter(p => p.id === id)[0]
+    const user = users.filter(u => u.id === pet.user_id)[0]
+    return fn({user, pet})
   }
 })(Pet)
 
@@ -120,83 +98,49 @@ function NotFound() {
   return <p>404 Not Found</p>
 }
 
-function navigate(path) {
-  history.pushState(null, "", path)
-  render({
-    ...state,
-    location: path
-  })
-}
-
-window.addEventListener('popstate', e => {
-  render({
-    ...state,
-    location: window.location.pathname
-  })
-})
-
-// function render(state) {
-  // ReactDOM.render(<Router {...state}>
-  //   <Route path="/" component={Index} />
-  //
-  //   <Route path="/users" component={Users}>
-  //     <Route path=":id" component={User} />
-  //   </Route>
-  //
-  //   <Route path="/pets" component={Pets}>
-  //     <Route path=":id" component={Pet} />
-  //   </Route>
-  //
-  //   <Route path="(.*)" component={NotFound} />
-  // </Router>, document.querySelector('#app'))
-// }
-
-function render(state) {
-  ReactDOM.render(<Router {...state}>
-    <Route path="/" component={Index}>
-      <Route path="users" component={Users}>
-        <Route path=":id" component={User} />
-      </Route>
-
-      <Route path="pets" component={Pets}>
-        <Route path=":id" component={Pet} />
-      </Route>
-    </Route>
-
-    <Route path="(.*)" component={NotFound} />
-  </Router>, document.querySelector('#app'))
-}
-
-// function render(state) {
-//   ReactDOM.render(<Router {...state}>
-//     <Route path="/" component={Index} />
-//     <Route path="/users" component={Users} />
-//     <Route path="/users/:id" component={User} />
-//     <Route path="/pets" component={Pets} />
-//     <Route path="/pets/:id" component={Pet} />
-//     <Route path="(.*)" component={NotFound} />
-//   </Router>, document.querySelector('#app'))
-// }
-
-
-// TODO: update readme examples
-// TODO: index / redirect
-// TODO: tests
-// TODO: server example
-
-const state = {
-  location: window.location.pathname,
+const Data = {
   users: [
-    { id: 1, name: 'Bob' },
-    { id: 2, name: 'Joe' },
+    {id: '1', name: 'Bob'},
+    {id: '2', name: 'Joe'},
   ],
   pets: [
-    { id: 1, user_id: 1, name: 'Tobi', species: 'Ferret' },
-    { id: 2, user_id: 1, name: 'Loki', species: 'Ferret' },
-    { id: 3, user_id: 1, name: 'Jane', species: 'Ferret' },
-    { id: 4, user_id: 2, name: 'Manny', species: 'Cat' },
-    { id: 5, user_id: 2, name: 'Luna', species: 'Cat' }
-  ]
+    {id: '1', user_id: '1', name: 'Tobi', species: 'Ferret'},
+    {id: '2', user_id: '1', name: 'Loki', species: 'Ferret'},
+    {id: '3', user_id: '1', name: 'Jane', species: 'Ferret'},
+    {id: '4', user_id: '2', name: 'Manny', species: 'Cat'},
+    {id: '5', user_id: '2', name: 'Luna', species: 'Cat'},
+  ],
 }
 
-render(state)
+function App() {
+  const [location, setLocation] = useState(window.location.pathname)
+  const navigate = useCallback(path => {
+    history.pushState(null, "", path)
+    setLocation(path)
+  }, [])
+  useEffect(() => {
+    window.addEventListener('popstate', () => {
+      setLocation(window.location.pathname)
+    })
+  }, [])
+
+  return (
+    <navigateContext.Provider value={navigate}>
+      <Router {...{location}} {...Data}>
+        <Route path="/" component={Index}>
+          <Route path="users" component={Users}>
+            <Route path=":id" component={User}/>
+          </Route>
+
+          <Route path="pets" component={Pets}>
+            <Route path=":id" component={Pet}/>
+          </Route>
+        </Route>
+
+        <Route path="(.*)" component={NotFound}/>
+      </Router>
+    </navigateContext.Provider>
+  )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'))
---
+++ example/webpack.config.js
@@ -0,0 +1,24 @@
+const Babel = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+  options: {
+    presets: ['@babel/preset-env'],
+    plugins: [
+      '@babel/plugin-transform-react-jsx',
+      '@babel/plugin-transform-runtime',
+    ],
+  },
+}
+
+module.exports = {
+  mode: 'development',
+  entry: './example/index.js',
+  module: {
+    rules: [Babel],
+  },
+  devServer: {
+    contentBase: './example',
+    historyApiFallback: true,
+  },
+}
