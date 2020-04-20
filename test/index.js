b3bdb051b9eb010e2cc44def20a001dbb7f2e01e
farwayer <farwayer@gmail.com>
2020-04-20 11:08:57 +0300

    test new syntax and features

--- test/index.js
+++ test/index.js
@@ -1,37 +1,38 @@
 import {equal} from 'assert'
 import React from 'react'
 import {renderToStaticMarkup} from 'react-dom/server'
-import { Router, Route } from '..'
+import {Router} from '..'
 
-function Index({ children }) {
+
+function Index({children}) {
   return <div>
     <h1>Index</h1>
     {children}
   </div>
 }
 
-function Users({ children }) {
+function Users({children}) {
   return <div>
     <h2>Users</h2>
     {children}
   </div>
 }
 
-function User({ children, params: { userId } }) {
+function User({userId, children}) {
   return <div>
     <h2>User {userId}</h2>
     {children}
   </div>
 }
 
-function Pets({ children }) {
+function Pets({children}) {
   return <div>
     <h2>Pets</h2>
     {children}
   </div>
 }
 
-function Pet({ params: { petId } }) {
+function Pet({petId}) {
   return <div>pet {petId}</div>
 }
 
@@ -39,140 +40,271 @@ function NotFound() {
   return <div>Not Found</div>
 }
 
-function List({ items }) {
-  return <ul>{items.map((item, i) => <li key={i}>{item}</li>)}</ul>
+function List({items}) {
+  return <ul>
+    {items.map((item, i) => (
+      <li key={i}>{item}</li>
+    ))}
+  </ul>
 }
 
-// Simple index route
-assert(<Router location="/">
-  <Route path="/" component={Index} />
-</Router>, <Index />)
 
-// Props
-assert(<Router location="/list" items={["foo", "bar", "baz"]}>
-  <Route path="/" component={Index}>
-    <Route path="list" component={List} />
-  </Route>
-</Router>, <Index>
-  <List items={["foo", "bar", "baz"]} />
-</Index>)
+assert(
+  "No location",
 
-// Route props
-assert(<Router location="/list">
-  <Route path="/list" component={List} items={["foo", "bar", "baz"]} />
-</Router>, <List items={["foo", "bar", "baz"]} />)
+  <Router location='/s'>
+    <Index/>
+  </Router>,
 
-// Nested route
-assert(<Router location="/pets">
-  <Route path="/" component={Index}>
-    <Route path="pets" component={Pets} />
-  </Route>
-</Router>, <Index><Pets /></Index>)
+  null,
+)
 
-// Deeply nested route
-assert(<Router location="/pets/12">
-  <Route path="/" component={Index}>
-    <Route path="pets" component={Pets}>
-      <Route path=":petId" component={Pet} />
-    </Route>
-  </Route>
-</Router>, <Index><Pets><Pet params={{ petId: 12 }}></Pet></Pets></Index>)
+assert(
+  "Simple index route",
 
-// Many deeply nested route
-assert(<Router location="/users/5">
-  <Route path="/" component={Index}>
-    <Route path="pets" component={Pets}>
-      <Route path=":petId" component={Pet} />
-    </Route>
+  <Router location='/'>
+    <Index/>
+  </Router>,
 
-    <Route path="users" component={Users}>
-      <Route path=":userId" component={User} />
-    </Route>
-  </Route>
-</Router>, <Index><Users><User params={{ userId: 5 }}></User></Users></Index>)
+  <Index/>,
+)
 
-// Catch-all
-assert(<Router location="/something">
-  <Route path="/" component={Index}>
-    <Route path="pets" component={Pets}>
-      <Route path=":petId" component={Pet} />
-    </Route>
+assert(
+  "Nested route",
 
-    <Route path="users" component={Users}>
-      <Route path=":userId" component={User} />
-    </Route>
-  </Route>
+  <Router location='/pets'>
+    <Index>
+      <Pets path='pets'/>
+    </Index>
+  </Router>,
 
-  <Route path="(.*)" component={NotFound} />
-</Router>, <NotFound />)
+  <Index>
+    <Pets/>
+  </Index>,
+)
 
-// Nested route but index route
-assert(<Router location="/">
-  <Route path="/" component={Index}>
-    <Route path="pets" component={Pets} />
-  </Route>
-</Router>, <Index />)
+assert(
+  "Route props",
 
-// Shallow routes
-assert(<Router location="/">
-  <Route path="/" component={Index} />
-  <Route path="/users" component={Users} />
-</Router>, <Index />)
+  <Router location='/list'>
+    <List path='/list' items={['foo', 'bar', 'baz']}/>
+  </Router>,
 
-assert(<Router location="/users">
-  <Route path="/" component={Index} />
-  <Route path="/users" component={Users} />
-</Router>, <Users />)
+  <List items={['foo', 'bar', 'baz']}/>,
+)
 
-assert(<Router location="/user/5">
-  <Route path="/" component={Index} />
-  <Route path="/users" component={Users} />
-  <Route path="/user/:userId" component={User} />
-</Router>, <User params={{ userId: 5 }} />)
+assert(
+  "Deeply nested route",
 
-// Many nested routes and params
-assert(<Router location="/users/5/pets/2">
-  <Route path="/" component={Index}>
-    <Route path="users" component={Users}>
-      <Route path=":userId" component={User}>
-        <Route path="pets" component={Pets}>
-          <Route path=":petId" component={Pet} />
-        </Route>
-      </Route>
-    </Route>
-  </Route>
-</Router>, <Index>
-  <Users>
-    <User params={{ userId: 5 }}>
-      <Pets>
-        <Pet params={{ petId: 2 }}>
-        </Pet>
+  <Router location='/pets/12'>
+    <Index>
+      <Pets path='pets'>
+        <Pet path=':petId'/>
       </Pets>
-    </User>
-  </Users>
-</Index>)
+    </Index>
+  </Router>,
 
-// Index route
-assert(<Router location="/">
-  <Route component={Index} />
-</Router>, <Index />)
+  <Index>
+    <Pets>
+      <Pet petId={12}/>
+    </Pets>
+  </Index>,
+)
 
-// Nested index route
-assert(<Router location="/">
-  <Route path="/" component={Index}>
-    <Route component={Pets} />
-  </Route>
-</Router>, <Index><Pets /></Index>)
+assert(
+  "Deeply nested route with path part ends with '/'",
 
-// Several nested index routes
-assert(<Router location="/">
-  <Route component={Index}>
-    <Route component={Pets} />
-  </Route>
-</Router>, <Index><Pets /></Index>)
+  <Router location='/pets/12'>
+    <Index>
+      <Pets path='pets/'>
+        <Pet path=':petId'/>
+      </Pets>
+    </Index>
+  </Router>,
 
-function assert(actual, expected) {
-  actual = renderToStaticMarkup(actual);
-  expected = renderToStaticMarkup(expected);
-  equal(actual, expected);
+  <Index>
+    <Pets>
+      <Pet petId={12}/>
+    </Pets>
+  </Index>,
+)
+
+assert(
+  "Many deeply nested route",
+
+  <Router location='/users/5'>
+    <Index>
+      <Pets path='pets'>
+        <Pet path=':petId'/>
+      </Pets>
+      <Users path='users'>
+        <User path=':userId'/>
+      </Users>
+    </Index>
+  </Router>,
+
+  <Index>
+    <Users>
+      <User userId={5}/>
+    </Users>
+  </Index>,
+)
+
+assert(
+  "Catch-all",
+
+  <Router location='/something'>
+    <Index>
+      <Pets path='pets'>
+        <Pet path=':petId'/>
+      </Pets>
+
+      <Users path='users'>
+        <User path=':userId'/>
+      </Users>
+    </Index>
+
+    <NotFound path='(.*)'/>
+  </Router>,
+
+  <NotFound/>,
+)
+
+assert(
+  "Nested route but index route",
+
+  <Router location='/'>
+    <Index>
+      <Pets path='pets'/>
+    </Index>
+  </Router>,
+
+  <Index/>,
+)
+
+assert(
+  "Shallow routes 1",
+
+  <Router location='/'>
+    <Index/>
+    <Users path='/users'/>
+  </Router>,
+
+  <Index/>,
+)
+
+assert(
+  "Shallow routes 2",
+
+  <Router location='/users'>
+    <Index/>
+    <Users path='/users'/>
+  </Router>,
+
+  <Users/>,
+)
+
+assert(
+  "Shallow routes 3",
+
+  <Router location='/users/5'>
+    <Index path='/'/>
+    <Users path='/users'/>
+    <User path='/users/:userId'/>
+  </Router>,
+
+  <User userId={5}/>,
+)
+
+assert(
+  "Many nested routes and params",
+
+  <Router location='/users/5/pets/2'>
+    <Index>
+      <Users path='users'>
+        <User path=':userId'>
+          <Pets path='pets'>
+            <Pet path=':petId'/>
+          </Pets>
+        </User>
+      </Users>
+    </Index>
+  </Router>,
+
+  <Index>
+    <Users>
+      <User userId={5}>
+        <Pets>
+          <Pet petId={2}/>
+        </Pets>
+      </User>
+    </Users>
+  </Index>,
+)
+
+assert(
+  "Nested index route with path '/'",
+
+  <Router location='/'>
+    <Index path='/'>
+      <Pets/>
+    </Index>
+  </Router>,
+
+  <Index>
+    <Pets/>
+  </Index>,
+)
+
+assert(
+  "Several nested index routes",
+
+  <Router location='/'>
+    <Index>
+      <Pets/>
+    </Index>
+  </Router>,
+
+  <Index>
+    <Pets/>
+  </Index>,
+)
+
+assert(
+  "Param overrides route property",
+
+  <Router location='/pets/12'>
+    <Index>
+      <Pets path='pets'>
+        <Pet path=':petId' petId={123}/>
+      </Pets>
+    </Index>
+  </Router>,
+
+  <Index>
+    <Pets>
+      <Pet petId={12}/>
+    </Pets>
+  </Index>,
+)
+
+assert(
+  "Options",
+
+  <Router location='/pets/12/' options={{strict: true}}>
+    <Index>
+      <Pets path='pets'>
+        <Pet path=':petId'/>
+      </Pets>
+    </Index>
+    <NotFound path='(.*)'/>
+  </Router>,
+
+  <NotFound/>,
+)
+
+function assert(test, actual, expected) {
+  actual = renderToStaticMarkup(actual)
+  expected = renderToStaticMarkup(expected)
+  equal(actual, expected, test)
 }
