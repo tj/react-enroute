34b7c4a0eb215b87418e3c532068678bf9de8cba
farwayer <farwayer@gmail.com>
2020-04-20 11:03:42 +0300

    new syntax and full refactoring

--- index.js
+++ index.js
@@ -1,118 +1,60 @@
-import {createElement, useMemo, Children} from 'react'
-import {pathToRegexp} from 'path-to-regexp'
+import {cloneElement, Children} from 'react'
+import {match} from 'path-to-regexp'
 
-/**
- * Router routes things.
- */
 
-export function Router({children, ...props}) {
-  assert(props.location, 'Router "location" property is missing')
-
-  const routes = useMemo(() => addRoutes(children), [])
-  return renderMatch(routes, props)
+export function Router({
+  location,
+  options,
+  children,
+}) {
+  const routes = addRoutes(children)
+  return renderMatch(routes, location, options)
 }
 
-/**
- * Route does absolutely nothing :).
- */
-
-export function Route() {
-  assert(false, 'Route should not be rendered')
-}
-
-function addRoutes(nested, parentRoute, routes = {}) {
-  Children.forEach(nested, el => {
-    const {path, component, children, ...routeProps} = el.props
-    assert(component, `Route "component" property is missing for path "${path}"`)
-
-    const render = (params, routerProps, children) => {
-      const finalProps = {...routerProps, ...routeProps, children, params}
-      const node = createElement(component, finalProps)
-
-      return parentRoute
-        ? parentRoute.render(params, routerProps, node)
-        : node
-    }
-
-    const route = createRoute(path, parentRoute, render)
+function addRoutes(elements, parent, routes = {}) {
+  Children.forEach(elements, element => {
+    const route = createRoute(element, parent)
     routes[route.path] = route
 
-    if (!children) return
+    const {children} = element.props
     addRoutes(children, route, routes)
   })
 
   return routes
 }
 
-/**
- * Create route.
- */
-
-function createRoute(path, parentRoute, render) {
-  path = cleanPath(normalizePath(path, parentRoute))
-
-  const route = {path, render, params: []}
-  route.regexp = pathToRegexp(path, route.params)
-  return route
+function createRoute(element, parent) {
+  let path = fullPath(element.props.path, parent)
+  path = cleanPath(path)
+  return {path, parent, element}
 }
 
-/**
- * Normalize path based on the parent.
- */
-
-function normalizePath(path, parentRoute) {
-  if (!path) return parentRoute ? parentRoute.path : '/'
-  if (path[0] === '/') return path  // "/" signifies an absolute route
-  if (parentRoute == null) return path  // no need for a join
-  return `${parentRoute.path}/${path}` // join
+function fullPath(path, parent) {
+  if (!path) return parent ? parent.path : '/' // index route
+  if (path[0] === '/') return path // absolute
+  if (!parent) return path // root
+  return `${parent.path}/${path}`
 }
-
-/**
- * Clean path by stripping subsequent "//"'s. Without this
- * the user must be careful when to use "/" or not, which leads
- * to bad UX.
- */
 
 function cleanPath(path) {
   return path.replace(/\/\//g, '/')
 }
 
-/**
- * Render match route.
- */
-
-function renderMatch(routes, routerProps) {
+function renderMatch(routes, location, options) {
   for (const path in routes) {
-    const route = routes[path]
-    const params = match(route, routerProps.location)
-    if (!params) continue
+    const result = match(path, options)(location)
+    if (!result) continue
 
-    return route.render(params, routerProps)
+    const route = routes[path]
+    return render(route, result.params)
   }
 
   return null
 }
 
-/**
- * Check route match location.
- * Returns params if match.
- */
+function render(route, params, children) {
+  const {element, parent} = route
+  const node = cloneElement(element, params, children)
 
-function match(route, location) {
-  const res = route.regexp.exec(location)
-  if (!res) return
-
-  return route.params.reduce((params, p, i) => {
-    const val = res[i + 1]
-    params[p.name] = isString(val) ? decodeURIComponent(val) : val
-    return params
-  }, {})
-}
-
-function isString(s) {
-  return typeof s === 'string'
-}
-
-function assert(e, msg) {
-  if (!e) throw new Error(`react-enroute: ${msg}`)
+  return parent ? render(parent, params, node) : node
 }
