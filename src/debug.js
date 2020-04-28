2f1b9a13c7e461d82afc7f7b83c9115535ae69bb
farwayer <farwayer@gmail.com>
2020-04-28 19:08:51 +0300

    debug->warnNotFound: show '/' for root index if checked location starts with '/'

--- src/debug.js
+++ src/debug.js
@@ -2,7 +2,14 @@ export function warnNotFound(routes, location) {
   if (process.env.NODE_ENV === 'production') return
 
   const tried = Object.keys(routes)
-    .map(path => `\n- ${path || '/'}`)
+    .map(path => {
+      // empty path is root index
+      // if checked location started with '/' show it as '/'
+      if (!path && location[0] === '/') {
+        path = '/'
+      }
+      return `\n- ${path}`
+    })
 
   console.warn(
     `[react-enroute] route for '${location}' not found. ` +
